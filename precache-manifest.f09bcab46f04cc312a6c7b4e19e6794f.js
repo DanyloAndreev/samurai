15d4afddf5c3cdd847795659f66fdbe984625302
danyloandreev <danyloandreiev@gmail.com>
2020-07-28 07:22:20 +0300

    Updates

---
+++ precache-manifest.f09bcab46f04cc312a6c7b4e19e6794f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc27d0bd6cbb62be48abd8afa7bf407b",
+    "url": "/samurai/index.html"
+  },
+  {
+    "revision": "dcb12696c2c814a33ed6",
+    "url": "/samurai/static/css/3.bf3f7b39.chunk.css"
+  },
+  {
+    "revision": "c95d06d8088e1f494bbe",
+    "url": "/samurai/static/css/4.2754aca6.chunk.css"
+  },
+  {
+    "revision": "04dfc553edc5db58805d",
+    "url": "/samurai/static/css/main.ca8dcdd1.chunk.css"
+  },
+  {
+    "revision": "8df6a6a3ac988ea26710",
+    "url": "/samurai/static/js/2.725d222d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samurai/static/js/2.725d222d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb12696c2c814a33ed6",
+    "url": "/samurai/static/js/3.cc517170.chunk.js"
+  },
+  {
+    "revision": "c95d06d8088e1f494bbe",
+    "url": "/samurai/static/js/4.6e61c6ec.chunk.js"
+  },
+  {
+    "revision": "04dfc553edc5db58805d",
+    "url": "/samurai/static/js/main.a2102455.chunk.js"
+  },
+  {
+    "revision": "f15705884dbed4a8c517",
+    "url": "/samurai/static/js/runtime-main.5dcafe4f.js"
+  },
+  {
+    "revision": "9447da79a550be925c3ab6da041baf9d",
+    "url": "/samurai/static/media/ColossalMeekCygnet-size_restricted.9447da79.gif"
+  },
+  {
+    "revision": "8fdbb18c64a9743eb9f5664e24d594b0",
+    "url": "/samurai/static/media/logo_720.8fdbb18c.png"
+  }
+]);
